6b3264e91005889786a51ee564264a0e98f9cb37
IsaacAu11 <iszac987@gmail.com>
2025-12-22 20:42:39 +0000

    Update lakefile.toml to rename project from "Untitled" to "SecurityNotation"

---
+++ Main.lean
@@ -0,0 +1,4 @@
+import SecurityNotation
+
+def main : IO Unit :=
+  IO.println "Hello, world!"
---
+++ SecurityNotation.lean
@@ -0,0 +1,4 @@
+import SecurityNotation.Basic.Syntax
+import SecurityNotation.Basic.Semantics
+import SecurityNotation.Basic.Notation
+import SecurityNotation.Basic.Utilities
